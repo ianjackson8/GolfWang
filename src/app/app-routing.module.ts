79952046e5b82f478e927e7ffca6b1c3fd172b08
Ian Jackson <iansjackson8@gmail.com>
2023-02-11 17:01:12 -0500

    added page title to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,21 @@ import { RetailComponent } from './retail/retail/retail.component';
 import { TourComponent } from './tour/tour/tour.component';
 
 const routes: Routes = [
-  {path: "", component: HomeLayoutComponent},
-  {path: "tour", component: TourComponent},
-  {path: "flag", component: RetailComponent}
+  {
+    path: "", 
+    component: HomeLayoutComponent,
+    title: "GOLF WANG"
+  },
+  {
+    path: "tour", 
+    component: TourComponent,
+    title: "Tour"
+  },
+  {
+    path: "flag",
+    component: RetailComponent,
+    title: "Flag"
+  }
 ];
 
 @NgModule({
